0831fa365bef1e6384fe1632ceeb8e9a93237809
Cam Wheeler <cameronwheeler@eftsource.com>
2014-10-03 15:07:28 -0500

    initial commit

---
+++ src/as.service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @as.service {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
